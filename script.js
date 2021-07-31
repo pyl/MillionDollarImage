d28252e98309f01df73685d7904d911662f2e298
Josephine Deng <jojod_27@yahoo.com>
2021-07-31 00:17:19 +0000

    create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function shareYourImage() {
+    let image = document.querySelector("image");
+    let link = document.querySelector("link");   
+}
+
+function renderDataAsHTML() {
+    document.querySelector("").innerHTML = image;
+}
