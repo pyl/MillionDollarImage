aff2a025796028c94e24bafd2a46e2ce308a6001
Paul Beltran <python8u@gmail.com>
2021-07-31 01:22:06 +0000

    added image resize

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function shareYourImage() {
-    let image = document.querySelector("image");
+
+function shareYourImage(imglink) {
+    var maxHeight = 500;
+    var maxWidth = 500;
+    var image = document.getElementById("milliondollarimage");
+    image.src = imglink;
     let link = document.querySelector("link");   
 }
+
+shareYourImage("https://www.paypalobjects.com/ppdevdocs/img/docs/checkout/pp-ecxo-integration-patterns.png");
 
 function renderDataAsHTML() {
     document.querySelector("").innerHTML = image;
