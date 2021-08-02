98560adba4eadfaae0508f46bade8ad97293c734
jollypolly123 <rachelle.hu123@gmail.com>
2021-08-02 23:43:09 +0000

    sort archive, toggle chat

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+const chatIcon = document.querySelector("#chatIcon");
+
+window.onload = event => {
+  const images = firebase.database().ref();
+  images.orderByChild('price').limitToLast(1).once("value", snapshot => {
+    const data = snapshot.val();
+    console.log(data);
+    // renderDataAsHtml(data);
+  });
+};
+
+chatIcon.addEventListener("click", function() {
+    const chatIconState = chatIcon.getAttribute("name");
+    if (chatIconState === "closed") {
+        chatIcon.setAttribute('name', 'opened');
+    } else {
+        chatIcon.setAttribute('name', 'closed');
+    }
+});
 
 function shareYourImage(imglink) {
     var maxHeight = 500;
@@ -9,6 +28,8 @@ function shareYourImage(imglink) {
 
 shareYourImage("https://www.paypalobjects.com/ppdevdocs/img/docs/checkout/pp-ecxo-integration-patterns.png");
 
-function renderDataAsHTML() {
-    document.querySelector("").innerHTML = image;
+
+function renderDataAsHTML(data) {
+    document.querySelector("#mainContainer").innerHTML = image;
 }
+
