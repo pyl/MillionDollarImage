6d1ff66c331150e91f84d16a68256f66df45c1ac
jollypolly123 <rachelle.hu123@gmail.com>
2021-08-03 01:00:06 +0000

    cleanup; toggle chat; etc lmao

--- js/script.js
+++ js/script.js
@@ -57,3 +57,114 @@ paypal.Buttons({
     }
 }).render('#paypal-button-container');
 //This function displays Smart Payment Buttons on your web page.
+
+
+
+// https://textboxwebsite.firebaseapp.com/
+// live chat
+var ref;
+var username = "lol";
+
+var firstmessage = 1;
+
+function setup() {
+  noCanvas();
+var database = firebase.database();
+ref = database.ref('text');
+ref.on('value', gotData, errData);
+
+document.getElementById("submitbutton").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        if (document.getElementById('submitbutton').value != "") {
+          if (firstmessage === 0) {
+          buttonClick();
+        } else if (firstmessage === 1) {
+          firstButtonClick();
+        }
+      }
+    }
+});
+
+}
+function gotData(data) {
+  document.getElementById("chat").innerHTML = "";
+  var scores = data.val();
+  var keys = Object.keys(scores)
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var text = scores[k].typing;
+    var timestamp = scores[k].date;
+    var un = scores[k].nametag;
+    displayText(text, timestamp, un);
+  }
+  //delete old data
+  if (keys.length > 40) {
+    ref.child(keys[0]).remove();
+  }
+}
+
+const prng = s => (typeof s!=='undefined'&&((l=s%2147483647)<=0&&(l+=2147483646)),((l=l*16807%2147483647)-1)/2147483646);
+
+function charToNumber (s, i) {
+    return parseInt(s.charAt(i), 36) - 9;
+}
+function sumChars (s) {
+    var i = s.length, r = 0;
+    while (--i >= 0) r += charToNumber(s, i);
+    return r;
+}
+
+function displayText(t, ts, u) {
+
+    var li = createElement("li", ts.fontcolor('rgb(' + prng(sumChars(u))*255 + ',' + prng(sumChars(u))*255 + ',' + prng(sumChars(u))*255 + ')') + " " + u.fontcolor('rgb(' + prng(sumChars(u))*255 + ',' + prng(sumChars(u))*255 + ',' + prng(sumChars(u))*255 + ')') + ": " + linkify(t));
+    li.style("font-size","20px");
+    li.style("font-family","Arial");
+
+    li.parent("chat");
+    if (checkURL(t)) {
+    var val = t,
+                src = val,
+                img = document.createElement('img');
+                img.src = src;
+                img.height = "250"
+                img.width = "250"
+                document.getElementById('chat').appendChild(img);
+
+    }
+
+}
+function linkify(text) {
+    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(urlRegex, function(url) {
+    return '<a href="' + url + '">' + url + '</a>';
+    });
+}
+
+function checkURL(url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+function errData(err) {
+  console.log("error")
+  console.log(err)
+}
+function buttonClick() {
+  var d = new Date().toLocaleString();
+  var data = {
+    typing: document.getElementById('submitbutton').value,
+    date: d,
+    nametag: username
+  }
+  ref.push(data);
+  document.getElementById('submitbutton').value = "";
+}
+function firstButtonClick() {
+  username = document.getElementById('submitbutton').value;
+  firstmessage = 0;
+  document.getElementById("submitbutton").setAttribute("placeholder", "Message");
+  document.getElementById('submitbutton').value = "";
+  var usernametext = createP("Username: " + username, 0, 100);
+  usernametext.style('text-align', 'right');
+  usernametext.style('font-size', '30');
+}
